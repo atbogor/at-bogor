95049858185b35055177cdc6e13dfa0a61cd45c3
eyin <valinaevelyn69651@gmail.com>
2024-05-08 14:11:37 +0700

    footer template

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,35 +1,80 @@
+<style>
+  body {
+    font-family: 'SF Pro Display', sans-serif;
+    font-weight: 500;
+  }
+  
+  .bg-light{
+    background-color: #224121 !important;
+  }
+
+  .navbar-brand, .navbar-nav, .nav-link {
+    color: #FFB49F !important;
+  }
+
+  .btn-primary {
+    background-color: #FFB49F !important;
+    border-color: #FFB49F !important;
+    color: #FB2000 !important;
+    width: 100px;
+  }
+</style>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">@Bogor</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="/home">Home</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
+
       <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="/gallery">Gallery</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="/ticket">Ticket</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="/blog">Blog</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="/testimonial">Testimonial</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+
+    <ul class="navbar-nav ms-auto">
+      @auth
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Welcome Back, Cath
+        </a>
+        <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="/dashboard">My Dashboard</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                  <button type="submit" class="dropdown-item">
+                    Logout
+                  </button>
+              </li>
+          </ul>
+      </li>
+      @else
+      <li class="nav-item">
+        <a class="btn btn-primary" href="/login">Log In</a>
+      </li>
+      @endauth
+    </ul>
+
   </div>
 </nav>
