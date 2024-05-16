f6a6d4a340f855ab79cd136b61d05f5add81e579
eyin <valinaevelyn69651@gmail.com>
2024-05-16 15:20:47 +0700

    test

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,6 +14,21 @@ class RegisterController extends Controller
             'title' => 'Register'
             // bisa tambahin active
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:25', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:8|max:255'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+        return redirect('/login')->with('success', 'Register success, please login.');
     }
 
 }
