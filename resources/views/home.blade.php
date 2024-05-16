a9011a3baea7467c8293b23078868a599c828ee4
MichaelScu <michaelscuderiatanudjaja@gmail.com>
2024-05-16 16:31:17 +0700

    MS nyoba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
-@extends('layouts.main')
-
-@section('container')
-
+@extends('layouts.main') @section('container')
+<div class="Home jumbotron">
+    <div class="z-0 position-relative">
+        <img
+            src="https://c0.wallpaperflare.com/preview/455/605/131/indonesia-puncak-pass-bogor-bogor-landscape.jpg"
+            alt=""
+        />
+    </div>
+</div>
 
 @endsection
