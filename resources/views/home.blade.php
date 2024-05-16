b2d536fe10ca77b523db2b74624b0044c0c365cb
MichaelScu <michaelscuderiatanudjaja@gmail.com>
2024-05-16 20:36:05 +0700

    coba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.main') @section('container')
-<div class="Home jumbotron">
+
+<link rel="stylesheet" href="/css/home.css">
+
+<div class="home-jumbotron">
     <div class="z-0 position-relative">
-        <img
-            src="https://c0.wallpaperflare.com/preview/455/605/131/indonesia-puncak-pass-bogor-bogor-landscape.jpg"
-            alt=""
-        />
+        <div class= "text-center">
+            <h1>Welcome to</h1>
+        </div>
+        <div class ="home-jumbotron-image">
+            <img
+            src="https://c0.wallpaperflare.com/path/455/605/131/indonesia-puncak-pass-bogor-bogor-landscape-f98ae4b6ab32591c7bcaee138aff1a02.jpg"
+            alt=""/>
+            
+        </div>
+        
+        
+        
     </div>
 </div>
 
