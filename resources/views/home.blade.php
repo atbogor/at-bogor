a4bcef402530d327604d83516292c24e1e5d5a6b
eyin <valinaevelyn69651@gmail.com>
2024-05-17 12:45:44 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,23 @@
 
 <link rel="stylesheet" href="/css/home.css">
 
+<style>
+    .welcome.text-center {}
+</style>
+
 <div class="home-jumbotron">
     <div class="z-0 position-relative">
-        <div class= "text-center">
+        <div class="welcome text-center align-center">
             <h1>Welcome to</h1>
         </div>
-        <div class ="home-jumbotron-image">
-            <img
-            src="https://c0.wallpaperflare.com/path/455/605/131/indonesia-puncak-pass-bogor-bogor-landscape-f98ae4b6ab32591c7bcaee138aff1a02.jpg"
-            alt=""/>
-            
+        <div class="home-jumbotron-image">
+            <img src="https://c0.wallpaperflare.com/path/455/605/131/indonesia-puncak-pass-bogor-bogor-landscape-f98ae4b6ab32591c7bcaee138aff1a02.jpg"
+                alt="" />
+
         </div>
-        
-        
-        
+
+
+
     </div>
 </div>
 
