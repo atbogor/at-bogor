ff29426bac9b1271792e180d2d173ca8039ca784
MichaelScu <michaelscuderiatanudjaja@gmail.com>
2024-05-19 14:34:47 +0700

    tolong
    width container buat popular ticket autis

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,70 @@
 
 <link rel="stylesheet" href="/css/home.css">
 
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+
+
 <section class="home-jumbotron">
     <img src="https://c0.wallpaperflare.com/path/455/605/131/indonesia-puncak-pass-bogor-bogor-landscape-f98ae4b6ab32591c7bcaee138aff1a02.jpg" alt="">
     <div class ="home-title text-center">
-        Welcome to
+        <p>Welcome to</p>
+        <p>@Bogor</p>
     </div>
-    
 </section>
+
+<section class="popular-destination container">
+
+<div class="popular-button container">
+        <div class="row">
+            <div class="col-12">
+                <div class="scrollable-container d-flex">
+                    <div class="item-container">
+                        <h1>Item 1</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 2</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 3</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 4</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 5</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 6</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 7</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 7</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 7</h1>
+                    </div>
+                    <div class="item-container">
+                        <h1>Item 7</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+          
+       
+</div> 
+
+</section>
+
+<section class="popular-article-container">
+
+</section>
+
+<section class="gallery-container">
+
+</section>
+
 
 @endsection
