57d153c854ad42641a307409989be2525e6c9425
MichaelScu <michaelscuderiatanudjaja@gmail.com>
2024-05-20 12:14:19 +0700

    hampir tamat
    tinggal nunggu ticket sama gallery buat card groupnya

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,46 +13,94 @@
     </div>
 </section>
 
-<div class="home-page ml-4 mr-4">
-<section class="popular-destination">
 
-<div class="popular-button">
+<div class="home-page ml-4 mr-4 d-flex flex-column gap-5">
+    <section class="popular-destination ">
+        <h1 class="popular-destination-title text-center mb-3">Popular Destination</h1>
         <div class="row">
             <div class="col-12">
-                <div class="scrollable-container d-flex">
-                    @foreach($posts->take(7) as $post)
-                    <div class="item-container card" style= "width: 24rem;">
+                <div class="scrollable-container d-flex gap-3">
+                @foreach($posts->take(7) as $post)
+                    <div class="destination-container card" style= "width: 24rem;">
                         <img src="https://source.unsplash.com/1600x900/?{{ $post->category->name }}" class="card-img-top"
                             alt="...">
                         <div class="card-body">
                             <a class="btn btn-secondary mb-2"
                                 href="#">{{ $post->category->name }}</a>
                             <h5 class="card-title">{{ $post->title }}</h5>
-                            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                                                                                                                                                                                                                                                                                                                                                                                                                                        the
-                                                                                                                                                                                                                                                                                                                                                                                                                                        card's content.</p> -->
-                            <p><a class="link-offset-2 link-underline link-underline-opacity-100" href="#"><u>Read
-                                        now</u></a>
+                            <p>
+                                <a class="link-offset-2 link-underline link-underline-opacity-100" href="#">
+                                    <u>Read now</u>
+                                </a>
+                            </p>
                         </div>
                     </div>
                     @endforeach
                 </div>
             </div>
         </div>
-    </div>
-          
-       
-</div> 
 
-</section>
 
-<section class="popular-article-container">
+    </section>
+    
 
-</section>
+    <section class="popular-article-container">
+        <h1 class="popular-article-title text-center mb-3">Popular Article</h1>
 
-<section class="gallery-container">
+        <div class="row">
+            <div class="col-12">
+                <div class="scrollable-container d-flex gap-3">
+                    @foreach($posts->take(7) as $post)
+                    <div class="article-container card" style= "width: 24rem;">
+                        <img src="https://source.unsplash.com/1600x900/?{{ $post->category->name }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <a class="btn btn-secondary mb-2"
+                                href="#">{{ $post->category->name }}</a>
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            
+                            <p>
+                                <a class="link-offset-2 link-underline link-underline-opacity-100" href="#">
+                                    <u>Read now</u>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+     
+    </section>
 
-</section>
+    <section class="gallery-container">
+        <h1 class="popular-gallery-title text-center  mb-3">Popular Gallery</h1>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="scrollable-container d-flex gap-3">
+                    @foreach($posts->take(7) as $post)
+                    <div class="gallery-container card" style= "width: 24rem;">
+                        <img src="https://source.unsplash.com/1600x900/?{{ $post->category->name }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <a class="btn btn-secondary mb-2"
+                                href="#">{{ $post->category->name }}</a>
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            
+                            
+                            <p>
+                                <a class="link-offset-2 link-underline link-underline-opacity-100" href="#">
+                                    <u>Read now</u>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 
 
