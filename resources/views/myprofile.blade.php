7e9d46075b9e148740cd1d5543bfba552ab2c5e1
MichaelScu <michaelscuderiatanudjaja@gmail.com>
2024-06-11 19:54:36 +0700

    front end

--- resources/views/myprofile.blade.php
+++ resources/views/myprofile.blade.php
@@ -3,8 +3,10 @@
 <link rel="stylesheet" href="/css/myprofile.css">
 
 <div class="my-profile-page ml-4 mr-4 mt-5 d-flex justify-content-evenly">
+    <div class="menu-container">
+
     
-    <div class="card p-3" style="width: 18rem;">
+    <div class="menu card p-3" style="width: 18rem;">
         <div class="card-header h3">
             Catherine Olivia Winanda
         </div>
@@ -16,31 +18,46 @@
             <hr class="my-1 w-100">
             <a href="#" class="list-group-item list-group-item-action">Log Out</a>
         </div>
-    </div>    
+    </div>
+    </div>
+    
     <div class="content col-9 card p-3">
         <div class="title h3 px-3 py-2">
             Personal Info
         </div>
         <div class="personal-info-container px-3 py-2 d-flex flex-column row-gap-3">
             <div class="personal-info-section">
-                <label class="form-label ">Full Name</label>
+                <label class="form-label h5">Full Name</label>
                 <input class="form-control " placeholder="Catherine Olivia Winanda">
             </div>
             <div class="personal-info-section">
-                <label class="form-label ">Username</label>
+                <label class="form-label h5">Username</label>
                 <input class="form-control " placeholder="ketrin">
             </div>
             <div class="email-dob-container d-flex justify-content-evenly gap-3">
 
                 <div class="personal-info-section flex-fill">
-                    <label class="form-label ">Email</label>
+                    <label class="form-label h5">Email</label>
                     <input class="form-control " placeholder="catherinewinanda@example.com">
                 </div>
                 <div class="personal-info-section flex-fill">
-                    <label class="form-label ">Date of birth</label>
-                    <input class="form-control " placeholder="Ini tanggal">
+                    <label class="form-label h5">Date of birth</label>
+                    <input type="date" class="form-control">
                 </div>
             </div>
+            <div class="personal-info-section d-flex flex-column">
+                <label class="form-label h5">Gender</label>
+                <div class="gender-button-container d-flex gap-3">
+                    <img src="{{ asset("assets/gender/gender-male-not-active.png") }}" alt="">
+                    <img src="{{ asset("assets/gender/gender-female-active.png") }}" alt="">
+                </div>
+            </div>
+            <div class="my-profile-save-cancel-button d-flex gap-3 ">
+                <button type="button" class="save-button btn px-3">Save</button>
+                <button type="button" class="cancel-button btn px-3">Cancel</button>
+            </div>
+
+
         </div>
     </div>
 
