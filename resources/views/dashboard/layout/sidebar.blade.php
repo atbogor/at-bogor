834f52700a86912e1d8ad8721c6457d82aeaada7
Cathh06 <127665445+Cathh06@users.noreply.github.com>
2024-07-08 15:50:27 +0700

    cathh

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -2,60 +2,40 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/css/sidebar.css">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/myprofile.css">
+    <title>Document</title>
 </head>
 
-<style>
-   .nav-link {
-      color: #224121;
-    }
-    .nav-link.active {
-      background-color: #224121 !important;
-      color: white;
-    }
-</style>
+
 
 <body>
-  <!-- <main class="d-flex flex-nowrap"> -->
+    <div class="menu card px-3 py-2 mx-5 mt-5" style="width: 18rem;">
+        <div class="card-header h3">
+          <a href="/" class="d-flex justify-content-center align-items-center mb-3 mb-md-0 link-dark text-decoration-none">
+            <img class="mb-2" src="{{ asset('assets/atBogor-logo-reglog.png') }}" alt="" width="" height="57">
+          </a>
+        </div>
+        <div class="list-group d-flex row-gap-3 pb-4">
+            <hr class="my-1 w-100">
+            <a href="/dashboard/tickets" class="list-group-item list-group-item-action @yield('ticketActive')">
+              <i class="fa-solid fa-ticket mr-2" style="margin-right: 10px"></i>
+              Tickets
+            </a>
+            <a href="/dashboard/posts" class="list-group-item list-group-item-action @yield('blogActive')">
+              <i class="fa-solid fa-newspaper mr-2" style="margin-right: 10px"></i>
+              Blogs
+            </a>
+            <a href="/dashboard/galleries" class="list-group-item list-group-item-action @yield('galleryActive')">
+              <i class="fa-solid fa-image" style="margin-right: 10px"></i>
+              Galleries
+            </a>
+        </div>
+    </div>
 
-  <div class="inisidebar d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px; height: 100vh">
-    <a href="#" class="d-flex justify-content-center align-items-center mb-3 mb-md-0 link-dark text-decoration-none">
-      <img class="mb-2" src="{{ asset('assets/atBogor-logo-reglog.png') }}" alt="" width="" height="57">
-    </a>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item mb-2">
-        <a href="/dashboard/tickets" class="nav-link link-white @yield('ticketActive')" aria-current="page">
-          <i class="fa-solid fa-ticket mr-2"></i>
-          Tickets
-        </a>
-      </li>
-      <li class="nav-item mb-2">
-        <a href="/dashboard/posts" class="nav-link link-white @yield('blogActive')">
-          <i class="fa-solid fa-newspaper mr-2"></i>
-          Blogs
-        </a>
-      </li>
-      <li class="nav-item mb-2">
-        <a href="/dashboard/galleries" class="nav-link link-white @yield('galleryActive')">
-          <i class="fa-solid fa-image"></i>
-          Galleries
-        </a>
-      </li>
-    </ul>
-  </div>
-  <!-- </main> -->
-
-
-  <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-
-  <script src="sidebars.js">
 
     
-  </script>
 </body>
 
 </html>
--- resources/views/mybooking/layout/sidebar.blade.php
+++ resources/views/mybooking/layout/sidebar.blade.php
@@ -17,13 +17,23 @@
         </div>
         <div class="list-group d-flex row-gap-3">
             <hr class="my-1 w-100">
-            <a href="/mybooking/myprofile" class="list-group-item list-group-item-action @yield('myprofileActive')">My Profile</a>
-            <a href="/mybooking/mybookings" class="list-group-item list-group-item-action @yield('mybookingActive')">My Bookings</a>
-            <a href="/mybooking/mytestimonial" class="list-group-item list-group-item-action @yield('mytestimonialActive')">My Testimonial</a>
+            <a href="/mybooking/myprofile" class="list-group-item list-group-item-action @yield('myprofileActive')">
+                <i class="fa-regular fa-user" style="margin-right: 10px"></i>
+                My Profile
+            </a>
+            <a href="/mybooking/mybookings" class="list-group-item list-group-item-action @yield('mybookingActive')">
+                <i class="fa-solid fa-receipt" style="margin-right: 11px"></i>
+                My Bookings
+            </a>
+            <a href="/mybooking/mytestimonial" class="list-group-item list-group-item-action @yield('mytestimonialActive')">
+                <i class="fa-brands fa-rocketchat" style="margin-right: 6px"></i>
+                My Testimonial
+            </a>
             <hr class="my-0 w-100">
             <form action="/logout" method="post">
                 @csrf
                 <button type="submit" class="list-group-item list-group-item-action">
+                <i class="fa-solid fa-right-from-bracket" style="margin-right: 8px"></i>
                 Logout
                 </button>
             </form>
